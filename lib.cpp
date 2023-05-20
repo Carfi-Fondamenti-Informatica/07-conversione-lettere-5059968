4d86dad6af01ebe18ea5a4d1ba2b47a9dc73e8fa
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-20 12:51:08 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,19 +1,14 @@
-#include "lib.h"
+#include "paperino.h"
 
-bool condizione (char a){
+bool condizione (char&a){
     if((a>='a' and  a<='z') or (a>='A' and a<='Z')){
+        if((a>='a' and a<='z')){
+            a-=32;
+        } else if(a>='A' and a<='Z'){
+            a+=32;
+        }
         return true;
     } else{
         return false;
-    }
-}
-char conversione (char a){
-    char b;
-    if((a>='a' and a<='z')){
-        b=a-32;
-        return b;
-    } else if(a>='A' and a<='Z'){
-        b=a+32;
-        return b;
     }
 }
