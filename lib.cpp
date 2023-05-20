68afc3e4c2b0431db315b521fa254318602d990b
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-20 12:37:03 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -9,11 +9,11 @@ bool condizione (char a){
 }
 char conversione (char a){
     char b;
-    if((a>='a' and a>='z')){
-        b=a+32;
-        return b;
-    } else if(a>='A' and a>='Z'){
+    if((a>='a' and a<='z')){
         b=a-32;
+        return b;
+    } else if(a>='A' and a<='Z'){
+        b=a+32;
         return b;
     }
 }
